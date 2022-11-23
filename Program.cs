f60677faeea1503a7d9d36ce232408393e2399c4
«Roman <research777@mail.ru>
2022-11-23 18:51:40 +0300

    DRSEMINAR4_Ex1

--- Program.cs
+++ Program.cs
@@ -2,6 +2,29 @@
 //3, 5 -> 243 (3⁵)
 //2, 4 -> 16
 
+int NatDegree (int A, int B)
+{
+    int power = A;
+    for (int current = 1; current < B; current++)
+    {
+        power *= A;
+    }
+    return power;
+}
+
+Console.WriteLine("Input your first number: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input your second number: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberB <= 0)
+Console.WriteLine("You entered not a natural number of degree!Input a POSITIVE number");
+else
+{
+    int degree = NatDegree(numberA, numberB);
+    Console.WriteLine($"The number {numberA} to raise by power {numberB} will be {degree} ");
+}
+
 //Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 //452 -> 11
 //82 -> 10
