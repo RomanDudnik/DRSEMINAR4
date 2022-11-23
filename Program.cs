7be1e5a61dd7b2d9105c11bc325cfb44a2b03b5f
«Roman <research777@mail.ru>
2022-11-23 20:28:39 +0300

    DRSEMINAR4_Ex2

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 //3, 5 -> 243 (3⁵)
 //2, 4 -> 16
 
+/*
 int NatDegree (int A, int B)
 {
     int power = A;
@@ -17,18 +18,50 @@ int numberA = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Input your second number: ");
 int numberB = Convert.ToInt32(Console.ReadLine());
+
 if (numberB <= 0)
-Console.WriteLine("You entered not a natural number of degree!Input a POSITIVE number");
+Console.WriteLine("You entered not a natural number of degree!Input a POSITIVE second number");
 else
 {
     int degree = NatDegree(numberA, numberB);
     Console.WriteLine($"The number {numberA} to raise by power {numberB} will be {degree} ");
 }
+*/
 
 //Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 //452 -> 11
 //82 -> 10
 //9012 -> 12
+
+int SumDigit(int number)
+{
+    int sum = 0;
+    
+    while (number > 0)
+    {
+        sum = sum + number % 10;
+        number = number/10;
+
+    }
+    return sum;
+}
+
+Console.WriteLine("Input your number: ");
+int digits = Convert.ToInt32(Console.ReadLine());
+
+if (digits > 0)
+{
+    int sum = SumDigit(digits);
+    Console.WriteLine($"The sum of the digits of your number {digits} is a {sum} ");
+}
+else
+{   
+    int digitsNegative = digits*-1;
+    int sum = SumDigit(digitsNegative);
+    Console.WriteLine($"The sum of the digits of your number {digits} is a {sum} ");
+}
+
+
 
 //Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 //1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
