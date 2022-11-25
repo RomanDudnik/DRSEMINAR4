d9c22c23381c08c71c9b968d4b0735a921d890bb
«Roman <research777@mail.ru>
2022-11-25 12:30:08 +0300

    DRSEMINAR4_Ex1_v.3

--- Program.cs
+++ Program.cs
@@ -66,6 +66,45 @@ double degree = NatDegree(numberA, numberB);
 Console.WriteLine($"The number {numberA} to raise by power {numberB} will be {degree} ");
 */
 
+//Вариант 3 (работает с отрицательными степенями и целыми числами)
+
+/*
+int NatDegree (int A, int B)
+{
+    if (B >= 0)
+    {
+        int power = A;
+        for (int current = 1; current < B; current++)
+        {
+            power *= A;
+        }
+        return power;
+    }
+    else
+    {
+        int power = A;
+        for (int current = 1; current < B*-1; current++)
+        {
+            power *= A;
+        }
+        return power;
+    }
+}
+
+Console.WriteLine("Input your first number: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input your second number: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int degree = NatDegree(numberA, numberB);
+
+if (numberB >= 0)
+    Console.WriteLine($"The number {numberA} to raise by power {numberB} will be {degree} ");
+else
+    Console.WriteLine($"The number {numberA} to raise by power {numberB} will be 1/{degree} ");
+*/
+
 //Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 //452 -> 11
 //82 -> 10
